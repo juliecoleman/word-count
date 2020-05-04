9ba82ff4f14e45c2587c2dbfae166e68ea569857
Julie Coleman <juliecoleman820@gmail.com>
2020-05-04 19:35:11 +0000

    Function to count words in file created

--- wordcount.py
+++ wordcount.py
@@ -1,3 +1,21 @@
-words = {}
+def count_words_in_file(filename):
+
+    lines = open(filename)
+
+    word_counts = {}
 
 
+    for line in lines:
+
+        words = line.split(" ")
+
+        print(words)
+
+        for word in words:
+
+            word_counts[word] = word_counts.get(word, 0) + 1
+
+    return word_counts
+
+print(count_words_in_file("test.txt"))
+print(count_words_in_file("twain.txt"))
